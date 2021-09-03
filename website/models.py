6aab81750f57abcf59c309362db860d00acfb0cf
Stephen Cronin <cronin4392@gmail.com>
2021-09-03 17:40:49 -0400

    Add dish type

--- website/models.py
+++ website/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from autoslug import AutoSlugField
 
+class DishType(models.Model):
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(null=True, default=None, unique=True, populate_from='title')
+
+    def __str__(self):
+        return self.title
+
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
     slug = AutoSlugField(null=True, default=None, unique=True, populate_from='title')
     pub_date = models.DateTimeField('date published', auto_now_add=True)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
