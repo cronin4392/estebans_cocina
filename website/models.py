25a92030c0a63a84113bea4bd2fd711fba3562b8
Stephen Cronin <cronin4392@gmail.com>
2021-09-03 14:05:20 -0400

    Add auto slug field

--- website/models.py
+++ website/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
+    slug = AutoSlugField(null=True, default=None, unique=True, populate_from='title')
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     def __str__(self):
