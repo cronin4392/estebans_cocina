0dcd8927203a638a7f3262eff43a75a08ed13b8c
Stephen Cronin <cronin4392@gmail.com>
2021-09-03 11:28:01 -0400

    Load env file

--- estebans_cocina/settings.py
+++ estebans_cocina/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
 import os
 import sys
 import dj_database_url
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
