c605b32c30f317d43f6e5809020a60ccafc9d05b
Stephen Cronin <cronin4392@gmail.com>
2021-09-03 19:23:51 -0400

    Add ingredients to recipes

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
-from .models import Recipe, DishType
+from .models import Ingredient, Recipe, DishType
 
-admin.site.register([Recipe, DishType])
+class IngredientInline(admin.TabularInline):
+  model = Ingredient
+  extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+  inlines = [IngredientInline]
+  model = Recipe
+
+admin.site.register(DishType)
+admin.site.register(Recipe, RecipeAdmin)
--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from autoslug import AutoSlugField
+from django.core.exceptions import ValidationError
 
 class DishType(models.Model):
     title = models.CharField(max_length=200)
@@ -16,3 +17,26 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+class Ingredient(models.Model):
+    class Unit(models.TextChoices):
+        TBSP = 'TBSP', 'tbsp.'
+        TSP = 'TSP', 'tsp.'
+        OZ = 'OZ', 'oz'
+        LB = 'LB', 'lb'
+        CUP = 'CUP', 'cup'
+        BUNCH = 'BUNCH', 'bunch'
+
+    amount = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    unit = models.CharField(max_length=5, choices=Unit.choices, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def clean(self):
+        if self.unit and not self.amount:
+            raise ValidationError(
+                "You must provide a unit if there is an amount"
+            )
+
+    def __str__(self):
+        return self.name
